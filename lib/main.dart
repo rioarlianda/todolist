39c5ea7b35cd57661d94fb941bfd0e59b110babd
rioarlianda <109035120+rioarlianda@users.noreply.github.com>
2024-08-20 15:40:09 +0700

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:base_todolist/services/firebase_options.dart';
+import 'package:base_todolist/view/login_page.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/material.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatefulWidget {
+  const MyApp({super.key});
+
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Todo-List App',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginPage(),
+    );
+  }
+}
